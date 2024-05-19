779791de05ca8e9f985ae13a5740d7142e273698
Tripp Donnelly <tripp@wasteoftime.org>
2024-05-18 23:17:09 -0500

    production readiness

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ export const mysql = {
   core: {
     client: "mysql2",
     connection: {
-      host: "localhost",
+      host: process.env.MYSQL_HOST || "127.0.0.1",
       user: "framework",
       password: process.env.MYSQL_PASSWORD,
       database: "core",
--- docker/config/token.js
+++
@@ -1,7 +0,0 @@
-export default {
-  secret: process.env.SECRET,
-
-  options: {
-    expiresIn: "1h",
-  },
-};
