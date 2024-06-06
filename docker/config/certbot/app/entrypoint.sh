060cd57947b4bc2c922e10170f7de540e0e9d68f
Tripp Donnelly <tripp@wasteoftime.org>
2024-06-06 13:41:10 -0500

    tweaks

--- docker/config/certbot/app/entrypoint.sh
+++ docker/config/certbot/app/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+interruptible_sleep() {
+    local duration=$1
+    local i=0
+    while [ $i -lt $duration ]; do
+        sleep 1
+        i=$((i + 1))
+    done
+}
+
+cleanup() {
+    echo "Shutting down script..."
+    exit 0  # Exit cleanly
+}
+
+
+
+trap cleanup SIGTERM SIGINT
+
 echo "Certbot starting up...";
 sleep 1;
 if [ ! -f /certs/$DOMAIN_NAME/fullchain.pem ]; then
@@ -10,7 +28,9 @@ if [ ! -f /certs/$DOMAIN_NAME/fullchain.pem ]; then
     echo "Temporary self-signed certificate generated.";
 fi;
 echo "Sleeping for 60 seconds...";
-sleep 60; 
+#sleep 60; 
+interruptible_sleep 60;
+
 if [ ! -f /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem ]; then
     echo "Requesting new cert from Lets Encrypt...";
     certbot certonly -v --webroot --webroot-path /var/www/certbot -d $DOMAIN_NAME --force-renewal --non-interactive --email $CERTBOT_EMAIL --agree-tos --deploy-hook /app/post_hook.sh;
@@ -18,5 +38,6 @@ fi;
 while :; do 
     echo "Checking if any certs need renewal every 12 hours...";
     certbot renew;
-    sleep 12h; 
+    #sleep 12h; 
+    interruptible_sleep 43200;  # 12 hours
 done;
