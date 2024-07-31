afd142e00a1e6541f40b68385f1b363ad0c0df1d
Tripp Donnelly <tripp@wasteoftime.org>
2024-07-30 20:58:12 -0500

    debug stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,6 +35,35 @@ export default defineConfig({
       "/api": {
         target: `http://127.0.0.1:3001`,
         changeOrigin: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("open", (proxySocket) => {
+            console.log("proxy open", proxySocket);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log(
+              "Sending Request:",
+              req.method,
+              req.url,
+              " => TO THE TARGET =>  ",
+              proxyReq.method,
+              proxyReq.protocol,
+              proxyReq.host,
+              proxyReq.path,
+              JSON.stringify(proxyReq.getHeaders())
+            );
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url,
+              JSON.stringify(proxyRes.headers)
+            );
+          });
+        },
       },
     },
   },
