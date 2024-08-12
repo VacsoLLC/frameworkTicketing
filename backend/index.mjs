914e141244bf4017f4de476ed9ffd6e56387b172
Tripp Donnelly <tripp@wasteoftime.org>
2024-08-11 19:47:16 -0500

    print versions

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,6 +2,44 @@ import Backend, { loadFromDir } from "@vacso/frameworkbackend";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import fs from "fs/promises";
+
+async function getPackageVersion(packagePath) {
+  try {
+    const packageJson = await fs.readFile(packagePath, "utf-8");
+    const { version } = JSON.parse(packageJson);
+    return version;
+  } catch (error) {
+    console.error(`Error reading package.json from ${packagePath}:`, error);
+    return "unknown";
+  }
+}
+
+async function printVersions() {
+  const currentDirectory = dirname(fileURLToPath(import.meta.url));
+  const frameworkTicketingVersion = await getPackageVersion(
+    path.join(currentDirectory, "package.json")
+  );
+  const frameworkBackendVersion = await getPackageVersion(
+    path.join(
+      currentDirectory,
+      "node_modules/@vacso/frameworkbackend/package.json"
+    )
+  );
+  const frameworkFrontendVersion = await getPackageVersion(
+    path.join(
+      currentDirectory,
+      "../frontend/node_modules/@vacso/frameworkfrontend/package.json"
+    )
+  );
+
+  console.log("Package Versions:");
+  console.log(`frameworkTicketing: ${frameworkTicketingVersion}`);
+  console.log(`frameworkBackend: ${frameworkBackendVersion}`);
+  console.log(`frameworkFrontend: ${frameworkFrontendVersion}`);
+}
+
+printVersions();
 
 dotenv.config();
 
