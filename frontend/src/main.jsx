cbc0d07670c73d0583d2345bb68aa96e283baa91
Tripp Donnelly <tripp@wasteoftime.org>
2024-11-15 14:35:20 -0600

    fix tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
 import Frontend from "@vacso/frameworkfrontend";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Frontend />);
