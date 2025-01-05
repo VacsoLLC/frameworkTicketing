9b45da59b3a9bec2e1acc106b070f74361107a68
BhuvaneshPatil <bhuvaneshpatil80642@gmail.com>
2025-01-05 22:31:19 +0530

    address comments:

--- backend/config/signUp.js
+++ backend/config/signUp.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config(); // This line is necessary to load the .env file where your environment variables are defined.
 
 export default {
-    isEnabled: process.env.SIGNUP_ENABLED || true,
-    expiryInHours: process.env.SIGNUP_LINK_EXPIRY_IN_HOURS || 24
+    enabled: process.env.SIGNUP_ENABLED || true,
+    expiryInHours: process.env.SIGNUP_LINK_EXPIRY_IN_HOURS || 24,
+    allowedDomains: (process.env.ALLOWED_EMAIL_DOMAINS ?? '').split(',')
 }
